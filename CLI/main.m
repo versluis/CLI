3cfd4917534cf81c30ca90865cb0deb7b55b6802
Jay Versluis <jay@versluis.com>
2016-12-30 09:45:32 -0500

    Initial Commit

---
+++ CLI/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CLI
+//
+//  Created by Jay Versluis on 30/12/2016.
+//  Copyright © 2016 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
